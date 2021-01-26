ec9b3502f3b0a1f890b30295cb80494d96f2ec5c
jkjamies <jkjamies@mtu.edu>
2021-01-26 16:34:50 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # udacity.android.kotlin.developer.nanodegree.eggtimernotifications
 Egg Timer Notifications Application from Udacity Android Kotlin Developer Nanodegree program.
+
+Highlights:
+
+Notifications\
+NotificationManager\
+Notification Importance\
+Notification Channel (API 26)\
+AlarmManager\
+Notification Update/Cancel/CancelAll\
+Notification Styling (BigTextStyle, BigPictureStyle, InboxStyle, MediaStyle, MessagingStyle)\
+Notification Actions (Buttons on Notifications)\
+Notification Importance and Priority\
+Notification Badges and Removal\
+Firebase Cloud Messaging (push notifications)\
+Message Composing (comes with Firebase, Manifest, create channel, service [for token/etc])\
+Data Messages (Firebase)\
+\
+Notification - Foreground: onMessageReceived(), Background: System Tray\
+Data - Foreground: onMessageReceived(), Background: onMessageReceived()\
+Notification + Data - Foreground: onMessageReceived(), Background: Notification - System Tray and Data - onMessageReceived()\
+\
+Note: can make a notification internally from onMessageReceived() if foregrounded
---
+++ app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.eggtimernotifications
+
+import android.app.NotificationManager
+import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+//https://firebase.google.com/docs/cloud-messaging/?gclid=CKyh5Pmyl-QCFZ2UxQId5uEOHw
+//https://console.firebase.google.com/
+//http://support.google.com/firebase/answer/7015592
+//https://developers.google.com/android/guides/google-services-plugin
+//https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/FirebaseMessagingService
+//https://firebase.google.com/docs/reference/android/com/google/firebase/messaging/RemoteMessage
+//https://firebase.google.com/docs/cloud-messaging/android/topic-messaging#subscribe_the_client_app_to_a_topic
+//https://console.firebase.google.com/u/0/project/_/notification
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Log.d(TAG, "From: ${remoteMessage?.from}")
+
+        // TODO Step 3.5 check messages for data
+        // Check if message contains a data payload. Foreground (if in background would do notification)
+        remoteMessage?.data?.let {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+        }
+
+        // TODO Step 3.6 check messages for notification and call sendNotification
+        // Check if message contains a notification payload. Ensures user sees notification popping up if app in foreground.
+        remoteMessage?.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+            sendNotification(it.body!!)
+        }
+    }
+    // [END receive_message]
+
+    // TODO: Step 3.2 log registration token
+    // [START on_new_token]
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    override fun onNewToken(token: String?) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token)
+    }
+    // [END on_new_token]
+
+    /**
+     * Persist token to third-party (your app) servers.
+     *
+     * @param token The new token.
+     */
+    private fun sendRegistrationToServer(token: String?) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private fun sendNotification(messageBody: String) {
+        val notificationManager = ContextCompat.getSystemService(
+            applicationContext,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.sendNotification(messageBody, applicationContext)
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
